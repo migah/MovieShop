99af15f3cdb119d3d6790e367b01984e32ea789d
Grumme <jakobgrumsen@gmail.com>
2016-10-04 10:40:10 +0200

    Sidste update

--- MovieShopAdmin/Controllers/CustomersController.cs
+++ MovieShopAdmin/Controllers/CustomersController.cs
@@ -14,7 +14,7 @@ namespace MovieShopAdmin.Controllers
 {
     public class CustomersController : Controller
     {
-        private MovieShopContext db = new MovieShopContext();
+        //private MovieShopContext db = new MovieShopContext();
         private readonly IManager<Customer> customerManager = new DllFacade().GetCustomerManager();
 
 
@@ -53,8 +53,7 @@ namespace MovieShopAdmin.Controllers
             if (ModelState.IsValid)
             {
                 customerManager.Create(customer);
-               // db.Customers.Add(customer);
-               // db.SaveChanges();
+
                 return RedirectToAction("Index");
             }
 
@@ -64,10 +63,6 @@ namespace MovieShopAdmin.Controllers
         // GET: Customers/Edit/5
         public ActionResult Edit(int id)
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
             var customer = customerManager.Read(id);// db.Customers.Find(id);
 
             if (customer == null)
@@ -87,8 +82,7 @@ namespace MovieShopAdmin.Controllers
             if (ModelState.IsValid)
             {
                 customerManager.Update(customer);
-                //db.Entry(customer).State = EntityState.Modified;
-                //db.SaveChanges();
+
                 return RedirectToAction("Index");
             }
             return View(customer);
@@ -97,11 +91,8 @@ namespace MovieShopAdmin.Controllers
         // GET: Customers/Delete/5
         public ActionResult Delete(int id)
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            var customer = customerManager.Read(id);//db.Customers.Find(id);
+            var customer = customerManager.Read(id);
+
             if (customer == null)
             {
                 return HttpNotFound();
@@ -114,21 +105,11 @@ namespace MovieShopAdmin.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            var customer = customerManager.Read(id);//db.Customers.Find(id);
+            var customer = customerManager.Read(id);
 
             customerManager.Delete(id);
-            //db.Customers.Remove(customer);
-            //db.SaveChanges();
+        
             return RedirectToAction("Index");
         }
-
-        /*protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }*/
     }
 }
