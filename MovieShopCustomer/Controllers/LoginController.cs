c1bcc121a881ca7f9def4b11ee3233aa2e21ad1d
Grumme <jakobgrumsen@gmail.com>
2016-10-10 14:13:41 +0200

    Can check existing and create new users

---
+++ MovieShopCustomer/Controllers/LoginController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MovieShopDll;
+using MovieShopDll.Entities;
+
+namespace MovieShopCustomer.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly IManager<Customer> customerManager = new DllFacade().GetCustomerManager();
+
+        // GET: Checkout
+        public ActionResult Index()
+        {
+            //return RedirectToAction("Create");
+            return View();
+        }
+
+        // GET: Customers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Customers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,FirstName,LastName,Email, Address ")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                customerManager.Create(customer);
+
+                return RedirectToAction("Index");
+            }
+
+            return View(customer);
+
+
+        }
+
+        [ActionName("check")]
+        public ActionResult CheckUser(String username)
+        {
+            if (ModelState.IsValid)
+            {
+                var test = username;
+
+                var customers = customerManager.Read();
+
+                var customer = customers.FirstOrDefault(x => x.FirstName == username);
+
+                if (customer != null)
+                {
+                    return RedirectToAction("../Checkout");
+                } 
+                //customerManager.Create(customer);
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+    }
+}
---
+++ MovieShopCustomer/Models/CustomerMovieView.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieShopCustomer.Models
+{
+    public class CustomerMovieView
+    {
+    }
+}
