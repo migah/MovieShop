3d992d7b6c2e5264b4325fa6e9dfba9d36804898
Grumme <jakobgrumsen@gmail.com>
2016-10-10 12:11:13 +0200

    Last commit

--- MovieShopCustomer/Controllers/CheckoutController.cs
+++ MovieShopCustomer/Controllers/CheckoutController.cs
@@ -17,9 +17,53 @@ namespace MovieShopCustomer.Controllers
         // GET: Checkout
         public ActionResult Index()
         {
+            //return RedirectToAction("Create");
+             return View();
+        }
+
+        // GET: Customers/Create
+        public ActionResult Create()
+        {
             return View();
         }
 
+        // POST: Customers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,FirstName,LastName,Email, Address ")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                customerManager.Create(customer);
+
+                return RedirectToAction("Index");
+            }
+
+            return View(customer);
+        }
+
+        [ActionName("check")]
+        public ActionResult CheckUser(String username)
+        {
+            if (ModelState.IsValid)
+            {
+                var customers = customerManager.Read();
+
+                var customer = customers.FirstOrDefault(x => x.FirstName == username);
+
+                if (customer != null)
+                {
+                    return RedirectToAction("Test");
+                }
+                //customerManager.Create(customer);
+
+                // return RedirectToAction("Index");
+            }
+
+            return View();
+        }
 
     }
 }
